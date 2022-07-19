6e4c8bbb470ebf813467da93b2c5d5967f6baa22
Muhammad Haris Khan <developewithharis@gmail.com>
2022-07-19 13:10:09 +0500

    third commit

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,25 @@
-          
+/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+/*SUBSCRIBE HERE FOR API KEY: https://home.openweathermap.org/users/sign_up*/
+const apiKey = "4d8fb5b93d4af21d66a2948710284366";
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let inputVal = input.value;
+
+//   check if there is already city 
+form.listItems = list.querySelectorAll(".ajax-section .city");
+
+const listItemsArray = Array.form(listItems);
+
+if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter(el => {
+        let content = "";
+    })
+}
+
+
+});
