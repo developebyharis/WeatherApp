dddc12da173cc86aea6a2cac4d23090e88687181
Muhammad Haris Khan <developewithharis@gmail.com>
2022-07-20 08:48:48 +0500

    fourth commit

--- js/weather.js
+++ js/weather.js
@@ -10,16 +10,52 @@ form.addEventListener("submit", e => {
   e.preventDefault();
   let inputVal = input.value;
 
-//   check if there is already city 
-form.listItems = list.querySelectorAll(".ajax-section .city");
+  //check if there's already a city
+  const listItems = list.querySelectorAll(".ajax-section .city");
+  const listItemsArray = Array.from(listItems);
 
-const listItemsArray = Array.form(listItems);
-
-if (listItemsArray.length > 0) {
+  if (listItemsArray.length > 0) {
     const filteredArray = listItemsArray.filter(el => {
-        let content = "";
-    })
-}
+      let content = "";
+      //athens,gr
+      if (inputVal.includes(",")) {
+        //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
+        if (inputVal.split(",")[1].length > 2) {
+          inputVal = inputVal.split(",")[0];
+          content = el
+            .querySelector(".city-name span")
+            .textContent.toLowerCase();
+        } else {
+          content = el.querySelector(".city-name").dataset.name.toLowerCase();
+        }
+      } else {
+        //athens
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
+      }
+      return content == inputVal.toLowerCase();
+    });
 
+    if (filteredArray.length > 0) {
+      msg.textContent = `You already know the weather for ${
+        filteredArray[0].querySelector(".city-name span").textContent
+      } ...otherwise be more specific by providing the country code as well 😉`;
+      form.reset();
+      input.focus();
+      return;
+    }
+  }
 
+  //ajax here
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0]["icon"]
+      }.svg`;
+
+      
+    });
 });
