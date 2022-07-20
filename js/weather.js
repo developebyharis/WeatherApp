0cc28beb685399ee7f0c4af096a0c773545f51b5
Muhammad Haris Khan <developewithharis@gmail.com>
2022-07-20 09:05:29 +0500

    sixth commit

--- js/weather.js
+++ js/weather.js
@@ -56,30 +56,30 @@ form.addEventListener("submit", e => {
         weather[0]["icon"]
       }.svg`;
 
-      
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+      }">
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "Please search for a valid city 😩";
     });
-    const li = document.createElement("li");
-    li.classList.add("city");
-    const markup = `
-      <h2 class="city-name" data-name="${name},${sys.country}">
-        <span>${name}</span>
-        <sup>${sys.country}</sup>
-      </h2>
-      <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-      <figure>
-        <img class="city-icon" src="${icon}" alt="${
-      weather[0]["description"]
-    }">
-        <figcaption>${weather[0]["description"]}</figcaption>
-      </figure>
-    `;
-    li.innerHTML = markup;
-    list.appendChild(li);
-  })
-  .catch(() => {
-    msg.textContent = "Please search for a valid city 😩";
-  });
 
-msg.textContent = "";
-form.reset();
-input.focus();
+  msg.textContent = "";
+  form.reset();
+  input.focus();
+});
+
